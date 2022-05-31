66377d9c0a28932412e5a2a4e6f3e79ffaead166
a_szczepanik <a.szczepanik@adform.com>
2022-05-31 15:07:09 +0200

    Test checking numbers of meetings

--- src/test/scala/CalendarSpec.scala
+++ src/test/scala/CalendarSpec.scala
@@ -2,13 +2,26 @@ import org.scalatest._
 import flatspec._
 import matchers._
 
+import scala.collection.mutable.ArrayBuffer
+
 class CalendarSpec extends AnyFlatSpec with should.Matchers {
 
   val calendar = new Calendar
 
   val firstMeeting = Meeting("lunch", Time(12, 0), Time(13, 0))
+  val secondMeetings = Meeting("sprint-review", Time(10, 0), Time(12, 0))
+
+  val meetings = calendar.meetings += firstMeeting += secondMeetings
 
   it should "add meeting into calendar" in {
     calendar.addMeeting(firstMeeting)
+  }
+
+  it should "remove meeting from calendar" in {
+    calendar.removeMeeting(firstMeeting)
+  }
+
+  it should "print number of meetings in calendar" in {
+    meetings.size shouldBe 2
   }
 }
