7e48e13564a5ad4889afff76fc983527d472253a
a_szczepanik <a.szczepanik@adform.com>
2022-05-31 15:20:53 +0200

    Add logic to addMeeting function

--- src/main/scala/Calendar.scala
+++ src/main/scala/Calendar.scala
@@ -4,7 +4,10 @@ class Calendar {
 
   var meetings: ArrayBuffer[Meeting] = ArrayBuffer.empty[Meeting]
 
-  def addMeeting(newMeeting: Meeting) = meetings += newMeeting
+  def addMeeting(newMeeting: Meeting) = newMeeting match {
+    case meeting if (newMeeting == meetings) => println(s"Wrong, $meeting already exist")
+    case _ => meetings += newMeeting
+  }
 
   def removeMeeting(closeMeeting: Meeting) = meetings -= closeMeeting
 
